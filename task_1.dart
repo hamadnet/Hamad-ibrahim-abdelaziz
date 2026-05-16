aa5c9f28fa7d8ebd672a94428ad862b57bfdb17c
hamadnet <120016990+hamadnet@users.noreply.github.com>
2026-05-16 03:16:19 +0300

    Update task_1.dart

--- task_1.dart
+++ task_1.dart
@@ -1,1 +1,49 @@
+# section2.dart
+import 'dart:io';
 
+void main() {
+  print("Enter degree:");
+
+  int degree = int.parse(stdin.readLineSync()!);
+
+
+    switch (degree) {
+      case >= 95 && <= 100:
+        print("+A");
+        break;
+
+      case >= 90 && < 95:
+        print("-A");
+        break;
+
+      case >= 85 && < 90:
+        print("+B");
+        break;
+
+      case >= 80 && < 85:
+        print("-B");
+        break;
+
+      case >= 75 && < 80:
+        print("+C");
+        break;
+
+      case >= 70 && < 75:
+        print("-C");
+        break;
+
+      case >= 65 && < 70:
+        print("+D");
+        break;
+
+      case >= 60 && < 65:
+        print("-D");
+        break;
+
+      default:
+        print("F");
+    }
+   
+
+  
+  }
